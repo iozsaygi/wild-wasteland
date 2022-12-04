cfee6874d23a53eb7aa435ed10f93790e137901a
İsmail Özsaygı <devozsaygi@gmail.com>
2022-12-04 18:53:03 +0300

    Add: Added SDL2 references to the Visual Studio project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
+#include <SDL.h>
 #include <iostream>
 
-int main( int argc, char* argv[] )
+int main( int argc, char* argv[] ) 
 {
-	std::cout << "Entry point of Wild Wasteland" << std::endl;
+	SDL_Init( SDL_INIT_VIDEO );
+	SDL_Window* window;
+
+	window = SDL_CreateWindow( "An SDL2 window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN );
+
+	if ( window == NULL ) 
+	{
+		// In the event that the window could not be made...
+		std::cout << "Could not create window: " << SDL_GetError() << '\n';
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Delay( 3000 );
+	SDL_DestroyWindow( window );
+	SDL_Quit();
+
 	return 0;
 }
